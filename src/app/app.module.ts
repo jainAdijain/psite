46031acc735192298078baae2793f6a4df6ed0d7
jainAdijain <jain.adityajpr1990@gmail.com>
2020-06-19 22:33:21 +0530

    Changed header component, added empty blogs and home pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule
